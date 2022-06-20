25e64ccda10c9b3e84a855f4ccdec46beb604004
sumon paul <sumpaul100@gmail.com>
2022-06-20 20:11:39 +0600

    async, settimeout, apitry

---
+++ timeout.js
@@ -0,0 +1,13 @@
+function dosomthing() {
+  console.log("something");
+}
+console.log(23495);
+console.log(2343);
+console.log(4342);
+// setTimeout(() => {
+//   console.log("See you later");
+// }, 2000);
+setInterval(function () {
+  console.log("doing it");
+}, 1000);
+console.log(43212);
